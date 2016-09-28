a5b6fb3521532421894faca4d4f5390962b44df7
joshdickson40 <joshdickson40@gmail.com>
2016-09-28 16:38:37 -0400

    Revert print test

--- events/janus_sampleevh.c
+++ events/janus_sampleevh.c
@@ -547,7 +547,7 @@ static void *janus_sampleevh_handler(void *data) {
 			hmac_sha256(auth_secret, strlen(auth_secret), event_text, strlen(event_text), result, resultlen);
 
 
-			JANUS_LOG(LOG_INFO, "Sig length: %d\n", *resultlen);
+			// JANUS_LOG(LOG_INFO, "Sig length: %d\n", *resultlen);
 
 
 			JANUS_LOG(LOG_INFO, "SHA completed...\n");
